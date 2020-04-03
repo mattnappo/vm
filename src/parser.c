7bc8c75c45a53805958653845eeecdc7f698fa49
xoreo <mattnappo@gmail.com>
2020-04-03 16:34:14 -0400

    Combined compact code

--- src/parser.c
+++ src/parser.c
@@ -1,25 +1,6 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include "parser.h"
 
 /* ---------- BEGIN PARSER CODE ---------- */
-
-#define TOKEN_SIZE 8
-#define INSTRUCTION_LIMIT 100
-
-// read_file_return allows for both a buffer and the length of that buffer
-// to be returned by the read_file method.
-typedef struct read_file_return {
-    char *buffer;
-    long length;
-} read_file_return;
-
-// tokenize_return allows for tokens and the amount of tokens to be returned
-// via a single call to the tokenize() method.
-typedef struct parsed_file {
-    int *program;
-    int size;
-} parsed_file;
 
 // read_file reads a file as a char* from the disk given the filepath.
 read_file_return read_file(char *input_file)
@@ -128,3 +109,4 @@ parsed_file parse(char *input_file)
 }
 
 /* ---------- END PARSER CODE ---------- */
+
--- src/parser.h
+++ src/parser.h
@@ -1,6 +1,13 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define TOKEN_SIZE 8
+#define INSTRUCTION_LIMIT 100
+
 // read_file_return allows for both a buffer and the length of that buffer
 // to be returned by the read_file method.
 typedef struct read_file_return {
@@ -9,7 +16,14 @@ typedef struct read_file_return {
 } read_file_return;
 
 // tokenize_return allows for tokens and the amount of tokens to be returned
-// via a single call to the tokenize() method.
+// via a single call to the tokenize() method./ token
+typedef struct tokenize_return {
+    char **tokens;
+    unsigned int token_count;
+} tokenize_return;
+
+// parsed_file represents a parsed file, containing a program and its size,
+// ready to be executed by the virtual machine.
 typedef struct parsed_file {
     int *program;
     int size;
